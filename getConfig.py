3dacb2f2d4bb90ca2194435c370a1f4440f6bd1e
tbrownex <tom@brownex.us>
2022-12-06 12:44:26 +0000

    second commit

---
+++ getConfig.py
@@ -0,0 +1,25 @@
+''' A dictionary object holds key parameters such as:
+    - the location and name of the input data file
+    - the location and name of the log file
+    - the default logging level
+    - the number of months in the "lookback window" for identifying "wins"
+    - the number of months in the "lookback window" for identifying "losses"
+    - how many months to use for Test
+    - an indicator allowing execution in Test mode'''
+
+__author__ = "The Hackett Group"
+
+def getConfig():
+
+    d = {}
+    d["dataLoc"]    = "~/data/"
+    d["fileName"]  = "SUSY.csv.gz"
+    d["labelColumn"] = "class"
+    d["logLoc"]     = "/home/tbrownex/"
+    d["logFile"]    = "SUSY.log"
+    d["logDefault"] = "info"
+    d['valPct'] = 0
+    d["testPct"]   = 0.25
+    d['batchSize'] = 64
+    d['ShuffleBufferSize'] = 64
+    return d
